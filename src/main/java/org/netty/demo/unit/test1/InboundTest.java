c5aea7966b210ca57f39694178deee640d0cf307
“rainbowsky911” <zdwbmw@163.com>
2022-02-28 15:17:33 +0800

    EmbeddedChannel登录验证加入时失败

--- src/main/java/org/netty/demo/unit/test1/InboundTest.java
+++ src/main/java/org/netty/demo/unit/test1/InboundTest.java
@@ -8,15 +8,16 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelOutboundHandlerAdapter;
 import io.netty.channel.ChannelPromise;
 import io.netty.channel.embedded.EmbeddedChannel;
-import io.netty.handler.codec.string.StringDecoder;
-import io.netty.handler.codec.string.StringEncoder;
 import org.junit.jupiter.api.Test;
 import org.netty.demo.echo.ClientLoginHandler;
 import org.netty.demo.echo.ServerLoginHandler;
 import org.netty.demo.unit.codecs.ByteToIntegerDecoder;
 import org.netty.demo.unit.codecs.IntegerToByteEncoder;
+import org.netty.demo.unit.test2.FixedLengthFrameDecoder;
 
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @Title: InboundTest
@@ -30,32 +31,6 @@ public class InboundTest {
     public static void main(String[] args) {
         echoClientHandlerTest();
     }
-
-
-    /**
-     * 测试登录
-     */
-    @Test
-    public void testLogin() {
-        // 1、创建 EmbeddedChannel，并加入要测试ChannelHandler
-        EmbeddedChannel embeddedChannel = new EmbeddedChannel(
-                new ClientLoginHandler(),
-                new ServerLoginHandler()
-        );
-        //2、写入入站数据，验证入站逻辑
-        ByteBuf buf = Unpooled.copiedBuffer("Test EchoHandler input", Charset.forName("utf-8"));
-        embeddedChannel.writeInbound(buf);
-        if (buf.refCnt() != 0) {
-            buf.release();
-        }
-        //3、写入出站数据，验证出站逻辑
-        ByteBuf buf1 = Unpooled.copiedBuffer("Test EchoHandler output", Charset.forName("utf-8"));
-        embeddedChannel.writeOutbound(buf1);
-        if (buf1.refCnt() != 0) {
-            buf1.release();
-        }
-    }
-
 
     public static void echoClientHandlerTest() {
 
@@ -71,17 +46,86 @@ public class InboundTest {
                 //new OutboundHandler2()
         );
         //2、写入入站数据，验证入站逻辑
-        ByteBuf buf = Unpooled.copiedBuffer("4396", Charset.forName("utf-8"));
+        ByteBuf buf = Unpooled.copiedBuffer("4396", StandardCharsets.UTF_8);
         embeddedChannel.writeInbound(buf);
         if (buf.refCnt() != 0) {
             buf.release();
         }
         //3、写入出站数据，验证出站逻辑
-        ByteBuf buf1 = Unpooled.copiedBuffer("4399", Charset.forName("utf-8"));
+        ByteBuf buf1 = Unpooled.copiedBuffer("4399", StandardCharsets.UTF_8);
         embeddedChannel.writeOutbound(buf1);
         if (buf1.refCnt() != 0) {
             buf1.release();
         }
+    }
+
+    @Test
+    public void testFramesDecoded2() {
+        ByteBuf buf = Unpooled.buffer();
+        for (int i = 0; i < 9; i++) {
+            buf.writeByte(i);
+        }
+        ByteBuf input = buf.duplicate();
+        //TODO 此处会报一个异常,所以我们的登录handler不能在这里测试
+        EmbeddedChannel channel = new EmbeddedChannel(
+                new FixedLengthFrameDecoder(3)
+//                new ClientLoginHandler(),
+//                new ServerLoginHandler()
+        );
+
+
+        assertFalse(channel.writeInbound(input.readBytes(2)));
+        assertTrue(channel.writeInbound(input.readBytes(7)));
+        assertTrue(channel.finish());
+        ByteBuf read = channel.readInbound();
+        assertEquals(buf.readSlice(3), read);
+        read.release();
+
+        read = channel.readInbound();
+        assertEquals(buf.readSlice(3), read);
+        read.release();
+
+        read = channel.readInbound();
+        assertEquals(buf.readSlice(3), read);
+        read.release();
+
+        assertNull(channel.readInbound());
+        buf.release();
+    }
+
+    /**
+     * 测试登录
+     */
+    @Test
+    public void testLogin() {
+        ByteBuf buf = Unpooled.buffer();
+        for (int i = 0; i < 9; i++) {
+            buf.writeByte(i);
+        }
+
+
+        // 1、创建 EmbeddedChannel，并加入要测试ChannelHandler
+        EmbeddedChannel embeddedChannel = new EmbeddedChannel(
+                new ClientLoginHandler(),
+                new ServerLoginHandler()
+        );
+
+        embeddedChannel.writeInbound(buf.retain());
+        embeddedChannel.finish();
+
+
+        //2、写入入站数据，验证入站逻辑
+//        ByteBuf buf = Unpooled.copiedBuffer("Test EchoHandler input", Charset.forName("utf-8"));
+//        embeddedChannel.writeInbound(buf);
+//        if (buf.refCnt() != 0) {
+//            buf.release();
+//        }
+//        //3、写入出站数据，验证出站逻辑
+//        ByteBuf buf1 = Unpooled.copiedBuffer("Test EchoHandler output", Charset.forName("utf-8"));
+//        embeddedChannel.writeOutbound(buf1);
+//        if (buf1.refCnt() != 0) {
+//            buf1.release();
+//        }
     }
 
 }
